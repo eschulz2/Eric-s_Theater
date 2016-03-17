24321690163ee3deb2430e0a408f140df226958f
eschulz2 <eschulz2@gmail.com>
2016-03-17 16:44:49 -0500

    back to deliver now for mail

--- app/controllers/showings_controller.rb
+++ app/controllers/showings_controller.rb
@@ -29,7 +29,7 @@ class ShowingsController < ApplicationController
   		showing = @ticket.showing
   		showing.seat_count -= 1
   		showing.save
-  		TicketMailer.receipt(@ticket).deliver_later
+  		TicketMailer.receipt(@ticket).deliver_now
   		flash[:notice] = "Ticket Purchased!"
   		redirect_to :root
   	else
