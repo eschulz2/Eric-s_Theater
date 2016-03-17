e1ac6d701bcb29cf26b65db35537d4d8213c622f
eschulz2 <eschulz2@gmail.com>
2016-03-17 16:05:20 -0500

    view added to display tickets sold to particular showings

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
 
   root 'visitors#index'
-  resources :showings
+  resources :showings do
+    member do
+      get 'tickets_sold'
+    end
+  end
+  get "/admin" => 'admins#index'
   post "/tickets" => 'showings#make_ticket'
   
 end
