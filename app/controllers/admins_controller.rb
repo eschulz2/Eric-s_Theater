f371c3c4773f8febd26be239a543d5db16d88371
eschulz2 <eschulz2@gmail.com>
2016-03-17 17:02:08 -0500

    reversed order for past showings on admin page

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,6 +1,6 @@
 class AdminsController < ApplicationController
   def index
     @upcoming_showings = Showing.order("time ASC").where(["time > ?", Time.now])
-    @past_showings = Showing.order("time ASC").where(["time < ?", Time.now])
+    @past_showings = Showing.order("time DESC").where(["time < ?", Time.now])
   end
 end
