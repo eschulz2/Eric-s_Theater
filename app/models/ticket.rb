c35a65ec9701da3890a6b8f3d6316e24454e76fa
eschulz2 <eschulz2@gmail.com>
2016-03-17 17:32:26 -0500

    min and max for ticket attribute lengths

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,7 +1,7 @@
 class Ticket < ActiveRecord::Base
 	belongs_to :showing
 
-	validates :name, :cc_exp, presence: true, length: { minimum: 4 }
+	validates :name, :cc_exp, presence: true, length: { minimum: 4, maximum: 20 }
 	validates :email, presence: true, format: { with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/ }
-	validates :cc_num, presence: true, length: { minimum: 8 }
+	validates :cc_num, presence: true, length: { minimum: 6, maximum: 20 }
 end
